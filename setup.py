eae4d9cc28cbb7786cfd8660b78496da6e0a2655
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-04-21 20:28:55 +0530

    v1.0.1

--- setup.py
+++ setup.py
@@ -1,30 +1,45 @@
-from setuptools import find_packages, setup
+import os
+import subprocess
+
+from setuptools import Extension, find_packages, setup
+from setuptools.command.build_ext import build_ext
+
+name = "sqlidps"
+
+
+tokenizer_module = Extension(
+    name="sqlidps.sql_tokenizer",
+    sources=["./sqlidps/lex.yy.c", "./sqlidps/wrapper.c"],
+    include_dirs=["./sqlidps"],
+    headers=["./sqlidps/wrapper.h"],
+    extra_compile_args=["-fPIC", "-O2", "-Wall"],
+    extra_link_args=["-bundle", "-undefined", "dynamic_lookup"],
+    language="c",
+)
 
 setup(
     name="sqlidps",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
-    install_requires=[
-        "joblib", "scikit-learn", "pandas", "numpy", "setuptools"
-        ],
-    include_package_data=True,  
+    install_requires=["numpy"],
+    ext_modules=[tokenizer_module],
+    include_package_data=True,
     package_data={
-        "sqlidps": [    
-            "inference.py",
-            "model.pkl",
-            "sql_tokenizer.so",
-        ],
-    },
-    author="Darisi Priyatham, Arjun Manjunath",
-    author_email="priyathamdarisi@gmail.com, dev.arjunmnath@gmail.com",
-    description="A Simple SQL injection detection and prevention package using ML",
+        "sqlidps": ["model.npz", "wrapper.h"]
+    },  # Ensure wrapper.h is included
+    # cmdclass={"build_ext": CustomBuildExt},
+    author="",
+    author_email="your.email@example.com",
+    description="A simple SQL-injection detector based on ML",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/DPRIYATHAM/sqlidps/",
+    url="https://github.com/DPRIYATHAM/sqli-dps/",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
 )

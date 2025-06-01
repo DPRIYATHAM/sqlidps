77514e38e91694392f5deae6b86ea1257df91e3e
Arjun Manjunath <dev.arjunmnath@gmail.com>
2025-06-01 17:55:36 +0530

    PyPI publish workflow fix (#1)
    * fix publish.yaml
    * added python versions 3.6 & 3.7

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 import os
-import subprocess
 import sys
 
 from setuptools import Extension, find_packages, setup
-from setuptools.command.build_ext import build_ext
 
 name = "sqlidps"
 
@@ -15,16 +13,27 @@ if sys.platform == "darwin":
     extra_compile_args += ["-fPIC", "-O2", "-Wall"]
 elif sys.platform.startswith("linux"):
     extra_compile_args += ["-fPIC", "-O2", "-Wall"]
+elif sys.platform.startswith("win"):
+    extra_compile_args += ["/O2", "/W3"]
 
 tokenizer_module = Extension(
     name="sqlidps.sql_tokenizer",
     sources=["./sqlidps/lex.yy.c", "./sqlidps/wrapper.c"],
     include_dirs=["./sqlidps"],
-    headers=["./sqlidps/wrapper.h"],
+    depends=["./sqlidps/wrapper.h"],
     extra_compile_args=extra_compile_args,
     extra_link_args=extra_link_args,
     language="c",
 )
+
+
+def read_readme():
+    try:
+        with open("README.md", "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        return "A simple SQL-injection detector based on ML"
+
 
 setup(
     name="sqlidps",
@@ -34,21 +43,36 @@ setup(
     ext_modules=[tokenizer_module],
     include_package_data=True,
     package_data={
-        "sqlidps": ["model.npz", "wrapper.h"]
+        "sqlidps": ["model.npz", "wrapper.h", "*.c"]  # Include C source files too
     },  # Ensure wrapper.h is included
+    zip_safe=False,
     # cmdclass={"build_ext": CustomBuildExt},
-    author="",
-    author_email="your.email@example.com",
+    author="Darisi Priyatham, Arjun Manjunath",
+    author_email="priyathamdarisi@gmail.com, dev.arjunmnath@gmail.com",
     description="A simple SQL-injection detector based on ML",
-    long_description=open("README.md").read(),
+    long_description=read_readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/DPRIYATHAM/sqli-dps/",
+    url="https://github.com/DPRIYATHAM/sqlidps/",
     classifiers=[
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: C",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
         "Operating System :: Microsoft :: Windows",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Security",
     ],
     python_requires=">=3.6",
+    keywords="sql injection detection security machine-learning",
+    project_urls={
+        "Bug Reports": "https://github.com/DPRIYATHAM/sqlidps/issues",
+        "Source": "https://github.com/DPRIYATHAM/sqlidps/",
+    },
 )
